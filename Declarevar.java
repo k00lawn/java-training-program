a32ba053871f03ba2c3883b1b958f03142e56d3e
k00lawn <k00lawn@gmail.com>
2021-09-01 21:29:21 +0530

    Java Programs

---
+++ Declarevar.java
@@ -0,0 +1,13 @@
+public class Declarevar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean a = false;
+		byte b = 10;
+		int c= 34;
+		float d = 67.2f;
+		double e = 12.3;
+		char f = 'A';
+		System.out.println(a + " " + b + " " + c + " " + d + " " + e + " " +f);
+	}
+
